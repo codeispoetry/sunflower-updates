d39824f47dd05a1fa7213578c425277744d7e248
Tom Rose <mail@tom-rose.de>
2021-03-26 06:42:02 +0100

    improve logging

--- code/log.php
+++ code/log.php
@@ -17,14 +17,22 @@ $latestVersion = getLatestVersion();
 
 
 
-$results = $db->query('SELECT *, julianday("now") - julianday(last_request_time) AS daysSinceRequest FROM websites ORDER BY installed_sunflower_version DESC;');
+$results = $db->query('SELECT *, julianday("now") - julianday(last_request_time) AS daysSinceRequest FROM websites WHERE daysSinceRequest < 30 ORDER BY installed_sunflower_version DESC;');
 
 $i = 1;
+$hosts = [];
+
 while ($row = $results->fetchArray()) {
 
     if(preg_match('/(localhost)|(127.0.0.1)/', $row['url'] )){
         continue;
     }
+
+    $host = parse_url( $row['url'], PHP_URL_HOST );
+
+    if(in_array($host, $hosts)){
+        continue;
+    }    
 
     printf('<tr style="background-color: %s"><td>%d</td><td><a href="%s" style="color:blue">%s</a></td><td>%s</td><td>%s</td></tr>',
         ($latestVersion == $row['installed_sunflower_version']) ? '#99ff00' : 'none',
@@ -35,6 +43,8 @@ while ($row = $results->fetchArray()) {
         round($row['daysSinceRequest'])
     );
     $i++;
+
+    $hosts[] = $host;
 }
 
 
